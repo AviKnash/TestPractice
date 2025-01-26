3e79101f859b6745e83cc17b5e2691f0a7b81190
AviKnash <bakaanash@yahoo.com>
2025-01-26 19:26:32 +0530

    Test more

--- sum.test.js
+++ sum.test.js
@@ -1,4 +1,4 @@
-test("Mock implementation of a basic function",() => {
-    const mock = jest.fn(x => 42 + x);
-    expect(mock(1)).toBe(43)
-})
+// test("Mock implementation of a basic function",() => {
+//     const mock = jest.fn(x => 42 + x);
+//     expect(mock(1)).toBe(43)
+// })
